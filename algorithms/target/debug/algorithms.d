104f00192c8ab95db08d232deafc543572147268
Taylor <96550502+taylors42@users.noreply.github.com>
2024-03-11 20:28:10 -0300

    .

---
+++ algorithms/target/debug/algorithms.d
@@ -0,0 +1,1 @@
+C:\Users\Tay\Documents\GitHub\rust-algorithms\algorithms\target\debug\algorithms.exe: C:\Users\Tay\Documents\GitHub\rust-algorithms\algorithms\src\main.rs
---
+++ algorithms/target/debug/deps/algorithms.d
@@ -0,0 +1,5 @@
+C:\Users\Tay\Documents\GitHub\rust-algorithms\algorithms\target\debug\deps\algorithms.exe: src\main.rs
+
+C:\Users\Tay\Documents\GitHub\rust-algorithms\algorithms\target\debug\deps\algorithms.d: src\main.rs
+
+src\main.rs:
